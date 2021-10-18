47d5ea3d54d27d705ba4f8a9e6901d529e012779
Sevda <74216005+sevdagulbaran@users.noreply.github.com>
2021-10-19 01:04:20 +0300

    webcam acıldı

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef , useEffect} from 'react';
 
 function App() {
+
+  const video = useRef(null);
+
+  // ilk çalıstırdıgımda
+useEffect(() => {
+  navigator.mediaDevices.getUserMedia({
+    video: {
+      width: 300,
+      height: 300,} })
+      .then(stream =>{
+        video.current.srcObject = stream;
+        console.log("kamera acıldı..");
+      }).catch(err=> console.log(err))
+
+}, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <video ref= {video}  autoplay muted/>
     </div>
   );
 }
